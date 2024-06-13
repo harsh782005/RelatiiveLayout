fa0843d7948c78ae5cef32cb706b878b3db62b50
harsh782005 <harshdeepkaur453@gmail.com>
2024-06-13 09:15:35 +0530

    Update MainActivity2.kt

--- app/src/main/java/com/harsh/relatiivelayout/MainActivity2.kt
+++ app/src/main/java/com/harsh/relatiivelayout/MainActivity2.kt
@@ -9,17 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Toast
+
 class MainActivity2 : AppCompatActivity() {
 
-
+    var btnNumber: Button? = null
+    var btnAdd: Button? = null
+    var btnSub: Button? = null
+    var btnMul: Button? = null
+    var btnDiv: Button? = null
+    var number = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
-        var btnNumber : Button?= null
-        var btnAdd : Button?= null
-        var btnSub : Button?= null
-        var btnMul : Button?= null
-        var btnDiv : Button?= null
-        var number = 0;
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
@@ -27,34 +26,29 @@ class MainActivity2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-       }
+        }
         btnNumber = findViewById(R.id.btnNumber)
         btnAdd = findViewById(R.id.btnAdd)
         btnSub = findViewById(R.id.btnMinus)
         btnMul = findViewById(R.id.btnMul)
         btnDiv = findViewById(R.id.btnDiv)
         btnAdd?.setOnClickListener {
-            number+=2
+            number += 2
             btnNumber?.setText(number.toString())
         }
         btnSub?.setOnClickListener {
-            number-=2
+            number -= 2
             btnNumber?.setText(number.toString())
         }
         btnDiv?.setOnClickListener {
-            number/=2
+            number /= 2
             btnDiv?.setText(number.toString())
         }
         btnMul?.setOnClickListener {
-            number*=2
+            number *= 2
             btnMul?.setText(number.toString())
         }
-
-
-     }
-    
-
+    }
 
 
 }
